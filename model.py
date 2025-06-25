ac11e4b3722f5d5ee4bd9d64cc48ef95c2137022
tqphu27 <trn.quangph27@gmail.com>
2025-06-25 11:26:29 +0700

    .

--- model.py
+++ model.py
@@ -165,8 +165,8 @@ hf_token = os.getenv("HF_TOKEN", "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")
 HfFolder.save_token(hf_token)
 
 
-hf_access_token = "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"
-login(token=hf_access_token)
+# hf_access_token = "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"
+# login(token=hf_access_token)
 CUDA_VISIBLE_DEVICES = []
 for i in range(torch.cuda.device_count()):
     CUDA_VISIBLE_DEVICES.append(i)
